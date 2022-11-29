b93a4aabc050b16501cdd963bb10288d3af402c4
sallanez <saul.alejandro19@gmail.com>
2022-11-29 16:53:35 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <h1>Hello World 😁</h1>
+  </>
+);
